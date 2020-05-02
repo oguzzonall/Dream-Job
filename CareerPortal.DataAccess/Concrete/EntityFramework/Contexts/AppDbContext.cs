832fbfdfa6cb7be17a714b8dfd6994aa32ceb3ad
Oğuz önal <oonal@nuevo.com.tr>
2020-05-03 00:50:59 +0300

    DbContext class was created.

---
+++ CareerPortal.DataAccess/Concrete/EntityFramework/Contexts/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using CareerPortal.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace CareerPortal.DataAccess.Concrete.EntityFramework.Contexts
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Experience> Experiences { get; set; }
+        public DbSet<Gender> Genders { get; set; }
+        public DbSet<JobPost> JobPosts { get; set; }
+        public DbSet<JobPostStatus> PostStatuses { get; set; }
+        public DbSet<JobType> JobTypes { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Sector> Sectors { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
