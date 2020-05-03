08b60ea64d7271ebb27d88f8abe0a3c11db1ffcf
Oğuz önal <oonal@nuevo.com.tr>
2020-05-03 18:10:30 +0300

    EfEntityRepository generic class created.

---
+++ CareerPortal.DataAccess/Concrete/EntityFramework/Repositories/EfEntityRepository.cs
@@ -0,0 +1,59 @@
+﻿using CareerPortal.Core.DataAccess.Abstract;
+using CareerPortal.Core.Entities.Abstract;
+using CareerPortal.DataAccess.Concrete.EntityFramework.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace CareerPortal.DataAccess.Concrete.EntityFramework.Repositories
+{
+    public class EfEntityRepository<TEntity> : IEntityRepository<TEntity>
+        where TEntity : class, IEntity, new()
+    {
+        private readonly AppDbContext _context;
+        private readonly DbSet<TEntity> _dbSet;
+
+        public EfEntityRepository(AppDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<TEntity>();
+        }
+
+        public void Add(TEntity entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public void AddRange(IEnumerable<TEntity> entities)
+        {
+            _dbSet.AddRange(entities);
+        }
+
+        public void Delete(TEntity entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public void DeleteRange(IEnumerable<TEntity> entities)
+        {
+            _dbSet.RemoveRange(entities);
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            return _dbSet.FirstOrDefault(filter);
+        }
+
+        public IList<TEntity> GetList(Expression<Func<TEntity, bool>> filter = null)
+        {
+            return filter == null ? _dbSet.ToList() : _dbSet.Where(filter).ToList();
+        }
+
+        public void Update(TEntity entity)
+        {
+            _dbSet.Update(entity);
+        }
+    }
+}
