95f037d1828e8ecee241ac8374df9e64edba9ee2
Oğuz önal <oonal@nuevo.com.tr>
2020-05-10 19:43:54 +0300

    Security Key Helper Added

--- CareerPortal.Core/Utilities/Security/Jwt/JwtHelper.cs
+++ CareerPortal.Core/Utilities/Security/Jwt/JwtHelper.cs
@@ -1,14 +1,25 @@
 ﻿using CareerPortal.Core.Entities.Concrete;
-using System;
+using CareerPortal.Core.Utilities.Security.Encryption;
+using Microsoft.Extensions.Configuration;
 using System.Collections.Generic;
 
 namespace CareerPortal.Core.Utilities.Security.Jwt
 {
     public class JwtHelper : ITokenHelper
     {
+        public IConfiguration Configuration { get; }
+        private TokenOptions _tokenOptions;
+
+        public JwtHelper(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            _tokenOptions = Configuration.GetSection("TokenOptions") as TokenOptions;
+        }
+
         public AccessToken CreateToken(User user, List<OperationClaim> operationClaims)
         {
-            throw new NotImplementedException();
+            var securityKey = SecurityKeyHelper.CreateSecurityKey(_tokenOptions.SecurityKey);
+            return new AccessToken();
         }
     }
 }
