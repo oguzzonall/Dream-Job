f1b217d822eb12103364435c83215652ce8864ba
Oğuz önal <oonal@nuevo.com.tr>
2020-05-10 03:18:07 +0300

    Role Management Data Access Configuration.

---
+++ CareerPortal.Core/DataAccess/Abstract/Dals/IUserDal.cs
@@ -0,0 +1,11 @@
+﻿using CareerPortal.Core.DataAccess.Abstract.Repositories;
+using CareerPortal.Core.Entities.Concrete;
+using System.Collections.Generic;
+
+namespace CareerPortal.Core.DataAccess.Abstract.Dals
+{
+    public interface IUserDal : IEntityRepository<User>
+    {
+        IList<OperationClaim> GetClaims(User user);
+    }
+}
---
+++ CareerPortal.DataAccess/Concrete/EntityFramework/Repositories/EfUserDal.cs
@@ -0,0 +1,37 @@
+﻿using CareerPortal.Core.DataAccess.Abstract.Dals;
+using CareerPortal.Core.Entities.Concrete;
+using CareerPortal.DataAccess.Concrete.EntityFramework.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CareerPortal.DataAccess.Concrete.EntityFramework.Repositories
+{
+    public class EfUserDal : EfEntityRepositoryBase<User>, IUserDal
+    {
+        private AppDbContext _appDbContext { get => _context as AppDbContext; }
+
+        public EfUserDal(AppDbContext context) : base(context)
+        {
+
+        }
+
+        public IList<OperationClaim> GetClaims(User user)
+        {
+            //using (var context = new AppDbContext())
+            //{
+            //    var result = from operationClaim in context.OperationClaims
+            //                 join userOperationClaim in context.UserOperationClaims
+            //                 on operationClaim.Id equals userOperationClaim.OperationClaimId
+            //                 where userOperationClaim.UserId == user.Id
+            //                 select new OperationClaim
+            //                 {
+            //                     Id = operationClaim.Id,
+            //                     Name = operationClaim.Name
+            //                 };
+            //    return result.ToList();
+            //}
+            return _appDbContext.UserOperationClaims.Where(x => x.UserId == user.Id).Include(x => x.OperationClaim).Select(x => x.OperationClaim).ToList();
+        }
+    }
+}
