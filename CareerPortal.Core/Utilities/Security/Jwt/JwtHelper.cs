22377c49d16ffb6cbce11d9eac3871a210c7b987
Oğuz önal <oonal@nuevo.com.tr>
2020-05-17 14:07:27 +0300

    Writing CreateJwt Operations

--- CareerPortal.Core/Utilities/Security/Jwt/JwtHelper.cs
+++ CareerPortal.Core/Utilities/Security/Jwt/JwtHelper.cs
@@ -1,7 +1,12 @@
 ﻿using CareerPortal.Core.Entities.Concrete;
 using CareerPortal.Core.Utilities.Security.Encryption;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
 
 namespace CareerPortal.Core.Utilities.Security.Jwt
 {
@@ -9,17 +14,43 @@ namespace CareerPortal.Core.Utilities.Security.Jwt
     {
         public IConfiguration Configuration { get; }
         private TokenOptions _tokenOptions;
+        DateTime _accessTokenExpiration;
 
         public JwtHelper(IConfiguration configuration)
         {
             Configuration = configuration;
             _tokenOptions = Configuration.GetSection("TokenOptions") as TokenOptions;
+            _accessTokenExpiration = DateTime.Now.AddMinutes(_tokenOptions.AccessTokenExpiration);
         }
 
         public AccessToken CreateToken(User user, List<OperationClaim> operationClaims)
         {
             var securityKey = SecurityKeyHelper.CreateSecurityKey(_tokenOptions.SecurityKey);
+            var signingCredentials = SigningCredentialsHelper.CreateSigningCredentials(securityKey);
             return new AccessToken();
+        }
+
+        public JwtSecurityToken CreateJwtSecurityToken(TokenOptions tokenOptions, User user, SigningCredentials signingCredentials, List<OperationClaim> operationClaims)
+        {
+            var jwt = new JwtSecurityToken(
+                    issuer: tokenOptions.Issuer,
+                    audience: tokenOptions.Audience,
+                    expires: _accessTokenExpiration,
+                    notBefore: DateTime.Now,
+                    claims: SetClaims(user, operationClaims),
+                    signingCredentials: signingCredentials
+                );
+            return new JwtSecurityToken();
+        }
+
+        private IEnumerable<Claim> SetClaims(User user, List<OperationClaim> operationClaims)
+        {
+            var claims = new List<Claim>();
+            //claims.AddNameIdentifier(user.Id.ToString());
+            //claims.AddEmail(user.Email);
+            //claims.AddName($"{user.FirstName} {user.LastName}");
+            //claims.AddRoles(operationClaims.Select(x => x.Name).ToArray());
+            return claims;
         }
     }
 }
